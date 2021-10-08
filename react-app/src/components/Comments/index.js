3f130450bd8cced8432e604847fc410e4d1a151c
Amber <amberbancroft2@gmail.com>
2021-10-08 15:38:31 -0700

    finished for the day

--- react-app/src/components/Comments/index.js
+++ react-app/src/components/Comments/index.js
@@ -6,18 +6,18 @@ import { addComments, getComments, deleteComment } from '../../store/comment';
 import './CommentForm.css';
 
 
-const EditCommentForm = ( { video_id } ) => {
+const EditCommentForm = ({ video_id }) => {
+    const [errors, setErrors] = useState([]);
+    const [validationError, setValidationError] = useState([]);
+    const [content, setContent] = useState('');
+    const [newComment, setNewComment] = useState('');
+    const [showForm, setShowForm] = useState(false);
+    const [formId, setFormId] = useState(null);
+
+    const user = useSelector(state => state.session.user);
+    const comments = useSelector(state => state.comments);
     const dispatch = useDispatch();
     const history = useHistory();
-    const user = useSelector(state => state.session.user)
-    const [errors, setErrors] = useState([]);
-    const [validationError, setValidationError] = useState([])
-    const [content, setContent] = useState('');
-    const [newComment, setNewComment] = useState('')
-    const comments = useSelector(state => state.comments)
-
-    const [showForm, setShowForm] = useState(false)
-    const [formId, setFormId] = useState(null)
 
     useEffect(() => {
         dispatch(getComments())
@@ -33,7 +33,6 @@ const EditCommentForm = ( { video_id } ) => {
         if (!newErrors.length) {
             dispatch(update_comment(comment_id, content))
             setShowForm(false)
-
         }
         else {
             setErrors(newErrors)
@@ -62,74 +61,79 @@ const EditCommentForm = ( { video_id } ) => {
 
     const Btn = () => {
         if (newComment) {
-          return <button id= 'post-button' type= 'submit'> Post </button>
+            return <button id='post-button' type='submit'> Post </button>
         } else {
-          return <button id= 'disabled-post-btn' disabled> Post </button>
+            return <button id='disabled-post-btn' disabled> Post </button>
         }
-      }
+    }
 
     return (
         <>
-        <div className= 'Comments--container'>
-            {Object.values(comments)?.map((comment, i) =>
-                <div key= { i } >
-                    {video_id === comment?.video_Id && (
-                        <div className= 'comment-edit'>
-                            
-                            <div onClick={e => { history.push(`/users/${comment?.username}`); window.location.reload(true);}}>
-                                <img src={comment?.profile_url} id='profile-icon' alt='user_photo'/>
-                            </div>
-                            <div className='content-container'>
-                                <div onClick={e => { history.push(`/users/${comment?.username}`); window.location.reload(true);}}>
-                                    <div style={{cursor:'pointer'}}> { `${comment?.username}` } </div>
+            <div className='Comments--container'>
+                {Object.values(comments)?.map((comment, i) =>
+                    <div key={i}>
+                        {video_id === comment?.video_Id && (
+                            <div className='comment-edit'>
+
+                                {/* Commenter Information */}
+                                <div onClick={e => { history.push(`/users/${comment?.username}`); window.location.reload(true); }}>
+                                    <img src={comment?.profile_url} id='profile-icon' alt='user_photo' />
                                 </div>
-                                <div> { `${comment?.content}` } </div>
-                            </div>
+                                <div className='content-container'>
+                                    <div onClick={e => { history.push(`/users/${comment?.username}`); window.location.reload(true); }}>
+                                        <div style={{ cursor: 'pointer' }}> {`${comment?.username}`} </div>
+                                    </div>
+                                <div> {`${comment?.content}`} </div>
 
-
-                            {user?.id === comment?.poster_Id && (
-                                <div className='btn-container'>
-                                    <button className= 'edit-delete-btn' onClick= { () => openForm(comment) }> Edit </button>
-                                    <button className= 'edit-delete-btn' onClick= { () => dispatch(deleteComment(comment?.id)) }> Delete </button>
-
-                                    {showForm && comment.id === formId ?
-                                        <form onSubmit= { (e) => handleSubmit(comment.id, content, e) }>
-                                            <ul className=' form-errors'>
-                                                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                                            </ul>
-                                            <hr />
-                                            <input type= 'text' value= { content } onChange={ (e) => setContent(e.target.value) }/>
-                                            <button type= 'submit'> Update </button>
-                                        </form>
-                                    : null}
                                 </div>
-                            )}
-                        </div>
-                    )}
-                </div>
 
-            )}
+                                {/* Edit Comment Button */}
+                                {user?.id === comment?.poster_Id && (
+                                    <div className='btn-container'>
+                                        {showForm && comment.id === formId ?
+                                            <form onSubmit={(e) => handleSubmit(comment.id, content, e)}>
+                                                <input type='text' value={content} required onChange={(e) => setContent(e.target.value)}/>
+                                                <button type='submit'> Save </button>
+                                            </form>
+                                            : null}
+
+                                        <button className='edit-delete-btn' onClick={() => openForm(comment)}> Edit </button>
+                                        <button className='edit-delete-btn' onClick={() => dispatch(deleteComment(comment?.id))}> Delete </button>
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                    </div>
+
+                )}
             </div>
-            <form id='posting-comments' onSubmit= { userComment }>
-                <div className= 'comment-and-errors'>
+
+            {/* Posting Comments */}
+            <form id='posting-comments' onSubmit={userComment}>
+                <div className='comment-and-errors'>
                     <div className='comment-form-errors'>
                         {validationError.map((error, idx) => <div key={idx} > {error} </div>)}
                     </div>
                     <div>
                         <input
-                            className= 'comment'
-                            type= 'text'
-                            placeholder= 'Add Comment...'
-                            value= { newComment }
-                            onChange={ (e) => setNewComment(e.target.value)}
+                            className='comment'
+                            type='text'
+                            placeholder='Add Comment...'
+                            value={newComment}
+                            onChange={(e) => setNewComment(e.target.value)}
                         />
                     </div>
                 </div>
 
-                <Btn/>
+                <Btn />
             </form>
         </>
     );
 };
 
 export default EditCommentForm;
+
+{/* <ul className=' form-errors'>
+    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+</ul> */}
+{/* <hr /> */}
